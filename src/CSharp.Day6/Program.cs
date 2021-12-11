e92cba4c456be1949e91628941ce2930199cd9af
Kevin Smith <kev_bite@msn.com>
2021-12-11 18:26:26 +0000

    Few more days

---
+++ src/CSharp.Day6/Program.cs
@@ -0,0 +1,34 @@
+﻿var daysLeft = File.ReadAllLines("input.txt")
+    .Single()
+    .Split(',')
+    .Select(int.Parse)
+    .ToArray();
+
+Console.WriteLine("Part 1: " + Sum(daysLeft, 80));
+
+Console.WriteLine("Part 2: " + Sum(daysLeft, 256));
+
+long Sum(IEnumerable<int> enumerable, int days)
+{
+    var initial = enumerable.GroupBy(x => x)
+        .ToDictionary(x => x.Key, x => x.Count());
+    var daysCountMap = Enumerable.Range(0, 9)
+        .Select(x => initial.TryGetValue(x, out var count) switch
+        {
+            true => count,
+            false => 0L
+        })
+        .ToArray();
+
+    for (var day = 0; day < days; day++)
+    {
+        var zeroCount = daysCountMap[0];
+        Array.Copy(daysCountMap, 1, daysCountMap, 0, daysCountMap.Length - 1);
+
+        daysCountMap[8] = zeroCount;
+        daysCountMap[6] += zeroCount;
+    }
+
+    var l = daysCountMap.Sum(x => x);
+    return l;
+}
---
+++ src/CSharp.Day7/Program.cs
@@ -0,0 +1,22 @@
+﻿var positions = File.ReadAllLines("input.txt")
+    .Single()
+    .Split(',')
+    .Select(int.Parse)
+    .ToArray();
+    
+var min = positions.Min();
+var max = positions.Max();
+
+int CalculateFuel(Func<int, int> fuelSum)
+{
+    return Enumerable.Range(min, max - min + 1)
+        .Select(x => positions.Select(pos => pos > x ? pos - x : x - pos)
+            .Sum(fuelSum))
+        .Min();
+}
+
+Console.WriteLine("Part 1");
+Console.WriteLine(CalculateFuel(i => i));
+
+Console.WriteLine("Part 2");
+Console.WriteLine(CalculateFuel(i => Enumerable.Range(1, i).Sum()));
